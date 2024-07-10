c2e489617363a4f4a63fafa7925c569c0ff6c6a6
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-07-10 21:49:06 +0500

    intial commit

---
+++ src/assets/icons/StarIcon.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function StarIcon(props) {
+    return (
+  <svg width="32.335938" height="30.753418" viewBox="0 0 32.3359 30.7534" fill="none" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+  <desc>
+    Created with Pixso.
+  </desc>
+  <defs />
+  <path id="Star 14" d="M16.16 0L12.35 11.74L0 11.74L9.99 19L6.17 30.75L16.16 23.49L26.16 30.75L22.34 19L32.33 11.74L19.98 11.74L16.16 0Z" fill="#C4928F" fillOpacity={1.000000} fillRule="nonzero" />
+</svg>
+
+
+    );
+}
+
+export default StarIcon;
